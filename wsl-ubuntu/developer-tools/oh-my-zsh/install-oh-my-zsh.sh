c56579a9b384836d5b94db102269e8963adcd5d8
Adauto Mendes <adauto.mendes@inatel.br>
2025-12-16 15:20:26 -0300

    Disable Powerlevel9k configuration wizard

--- wsl-ubuntu/developer-tools/oh-my-zsh/install-oh-my-zsh.sh
+++ wsl-ubuntu/developer-tools/oh-my-zsh/install-oh-my-zsh.sh
@@ -18,5 +18,10 @@ fi
 if ! grep -q 'ZSH_THEME="powerlevel10k/powerlevel10k"' "$HOME/.zshrc"; then
     echo -e "\n\033[1;32m>>> Configuring .zshrc to use Powerlevel10k theme...\033[0m\n"
     sed -i 's/^ZSH_THEME=.*/ZSH_THEME="powerlevel10k\/powerlevel10k"/' "$HOME/.zshrc"
+fi
+
+# Disable Powerlevel9k configuration wizard
+if ! grep -q 'POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true' "$HOME/.zshrc"; then
+    echo -e "\n\033[1;32m>>> Disabling Powerlevel9k configuration wizard...\033[0m\n"
     echo 'POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true' >>! $HOME/.zshrc
 fi
