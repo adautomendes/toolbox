194f5f4b41f7c02980dc1f699d3f87f6439545e1
Adauto Mendes <adauto.mendes@inatel.br>
2025-12-16 15:48:03 -0300

    Add Powerlevel10k default config

--- wsl-ubuntu/developer-tools/oh-my-zsh/install-oh-my-zsh.sh
+++ wsl-ubuntu/developer-tools/oh-my-zsh/install-oh-my-zsh.sh
@@ -19,3 +19,7 @@ if ! grep -q 'ZSH_THEME="powerlevel10k/powerlevel10k"' "$HOME/.zshrc"; then
     echo -e "\n\033[1;32m>>> Configuring .zshrc to use Powerlevel10k theme...\033[0m\n"
     sed -i 's/^ZSH_THEME=.*/ZSH_THEME="powerlevel10k\/powerlevel10k"/' "$HOME/.zshrc"
 fi
+
+# Download default .p10k.zsh and .zshrc configuration files
+curl -o $HOME/.p10k.zsh https://raw.githubusercontent.com/adautomendes/toolbox/refs/heads/main/wsl-ubuntu/developer-tools/oh-my-zsh/default-config/.p10k.zsh
+curl -o $HOME/.zshrc https://raw.githubusercontent.com/adautomendes/toolbox/refs/heads/main/wsl-ubuntu/developer-tools/oh-my-zsh/default-config/.zshrc
