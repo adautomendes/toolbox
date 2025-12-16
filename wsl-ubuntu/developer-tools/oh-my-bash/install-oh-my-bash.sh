8c85ea37edf9bb81b2c64cc22e49e9db40f358fa
Adauto Mendes <adauto.mendes@inatel.br>
2025-12-16 18:52:42 -0300

    Added execution permission to all scripts

--- wsl-ubuntu/developer-tools/oh-my-bash/install-oh-my-bash.sh
+++ wsl-ubuntu/developer-tools/oh-my-bash/install-oh-my-bash.sh
@@ -4,12 +4,14 @@
 if [ ! -d "$HOME/.oh-my-bash" ]; then
     echo -e "\n\033[1;32m>>> Installing oh-my-bash...\033[0m\n"
 
-    # Bakcup existing .bashrc if it exists
+    # Backup existing .bashrc if it exists
     if [ -f "$HOME/.bashrc" ]; then
         cp "$HOME/.bashrc" "$HOME/.bashrc.backup_$(date +%Y%m%d%H%M%S)"
         echo "Backup of existing .bashrc created."
     fi
 
-    bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+    curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh | bash
+
+    # Set oh-my-bash theme to "lambda"
     sed -i 's/^OSH_THEME=.*/OSH_THEME="lambda"/' "$HOME/.bashrc"
 fi
