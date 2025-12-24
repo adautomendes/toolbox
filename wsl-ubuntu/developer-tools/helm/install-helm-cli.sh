e04f91c00cf8cf3b2e318bd97a3e24a42e6b74b7
Adauto Mendes <adauto.mendes@inatel.br>
2025-12-24 17:24:47 -0300

    Add scripts for kubectl and helm

---
+++ wsl-ubuntu/developer-tools/helm/install-helm-cli.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+INSECURE=''
+
+parse_command_line_arguments() {
+    while [[ $# -gt 0 ]]; do
+        case $1 in
+            --insecure)
+                INSECURE='--insecure'
+                shift
+                ;;
+            *)
+                echo "Unknown option: $1"
+                exit 1
+                ;;
+        esac
+    done
+
+    echo -e "\n\033[1;32m>>> Provided options: $INSECURE\033[0m\n"
+}
+
+parse_command_line_arguments "$@"
+
+# Install nodejs and npm via nvm
+HELM_VERSION=$(curl -s $INSECURE https://api.github.com/repos/helm/helm/releases/latest | jq -r .tag_name)
+
+mkdir -p $HOME/helm
+
+curl -s $INSECURE -L -o $HOME/helm/helm-${HELM_VERSION}-linux-amd64.tar.gz https://get.helm.sh/helm-${HELM_VERSION}-linux-amd64.tar.gz
+tar -xzvf $HOME/helm/helm-${HELM_VERSION}-linux-amd64.tar.gz -C $HOME/helm
+mv $HOME/helm/linux-amd64/helm $HOME/helm/helm
+
+rm -r $HOME/helm/linux-amd64
+rm $HOME/helm/helm-${HELM_VERSION}-linux-amd64.tar.gz
+
+# Append config to .bashrc
+echo -e "\n# Helm configuration" >> $HOME/.bashrc
+echo "export PATH=\$PATH:$HOME/helm" >> $HOME/.bashrc
---
+++ wsl-ubuntu/developer-tools/kubectl/install-kubectl.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+INSECURE=''
+
+parse_command_line_arguments() {
+    while [[ $# -gt 0 ]]; do
+        case $1 in
+            --insecure)
+                INSECURE='--insecure'
+                shift
+                ;;
+            *)
+                echo "Unknown option: $1"
+                exit 1
+                ;;
+        esac
+    done
+
+    echo -e "\n\033[1;32m>>> Provided options: $INSECURE\033[0m\n"
+}
+
+parse_command_line_arguments "$@"
+
+mkdir -p $HOME/kubernetes
+
+curl $INSECURE -LO -o "$HOME/kubernetes/kubectl" "https://dl.k8s.io/release/$(curl $INSECURE -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
+chmod +x $HOME/kubernetes/kubectl
+
+# Append config to .bashrc
+echo -e "\n# kubectl configuration" >> $HOME/.bashrc
+echo "export PATH=\$PATH:$HOME/kubernetes" >> $HOME/.bashrc
